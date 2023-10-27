96d66ef7235af4e48abc497f5e35df216c6ea145
Владислав <vlad.dauermein@gmail.com>
2023-10-28 00:35:21 +0500

    Final of 4nd day

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 /**
- * Возвращение переменных в логическом выражении
+ * Возвращение списка переменных в логическом выражении
  * @param string - логическое выражение
- * @returns {*[]} - переменные в логическом выражении
+ * @returns {*[]} - список переменных в логическом выражении
  */
 function variable(string) {
-    const alp = ['!', '&', '|']
-    const array = Array.from(string)
+    const array = Array.from(string);
+    const alp = ["!", "&", "|"]
     let elements = []
+
     for (const element of array) {
         if (alp.indexOf(element) === -1) {
             elements.push(element)
         }
     }
+
     return elements
 }
 
@@ -24,9 +26,10 @@ function make_truth_table(string) {
     const array = variable(string)
     const save = string
     let truth_table = {
-        'True': [],
-        'False': []
+        "True": [],
+        "False": []
     }
+
     for (let x = 0; x < 2; x++) {
         for (let y = 0; y < 2; y++) {
             for (let z = 0; z < 2; z++) {
@@ -37,20 +40,23 @@ function make_truth_table(string) {
                     string = string.replaceAll(array[2],z.toString())
                     string = string.replaceAll(array[3],w.toString())
                     let expression = eval(string)
+
                     if (expression === 1) {
-                        let changes = truth_table['True']
+                        let changes = truth_table["True"]
                         changes.push([x,y,z,w])
-                        truth_table['True'] = changes
+                        truth_table["True"] = changes
                     }
+
                     else {
-                        let changes = truth_table['False']
+                        let changes = truth_table["False"]
                         changes.push([x,y,z,w])
-                        truth_table['False'] = changes
+                        truth_table["False"] = changes
                     }
                 }
             }
         }
     }
+
     return truth_table
 }
 
@@ -63,16 +69,22 @@ function get_expression(){
     return input.value
 }
 
-const truth_table = make_truth_table(get_expression())
+/**
+ * Построение таблицы HTML
+ */
+function make_html_table() {
+    const truth_table = make_truth_table(get_expression())
+    document.querySelector(".center_table").innerHTML = `<table class="table"></table>`
 
-document.querySelector('.center_table').innerHTML = `<table class="table"></table>`
-for (key in a) {
-    let row = document.createElement('tr')
-    row.innerHTML = `<td colspan="2">${key}</td>`
-    document.querySelector('.center_table').appendChild(row)
-    for (let i=0; i < a[key].length; i++) {
-        let row = document.createElement('tr')
-        row.innerHTML = `<td>${a[key][i]}</td>`
-        document.querySelector('.center_table').appendChild(row)
+    for (let key in truth_table) {
+        let row = document.createElement("tr")
+        row.innerHTML = `<td colspan="2">${key}</td>`
+        document.querySelector(".center_table").appendChild(row)
+
+        for (let i = 0; i < truth_table[key].length; i++) {
+            let row = document.createElement("tr")
+            row.innerHTML = `<td>${truth_table[key][i]}</td>`
+            document.querySelector(".center_table").appendChild(row)
+        }
     }
 }
--- test.js
+++ test.js
@@ -1,23 +1,24 @@
-let input = '(x||y)&&z||w'
-
 /**
+ * Возвращение списка переменных в логическом выражении
  * @param string - логическое выражение
- * @returns {*[]} - переменные в логическом выражении
+ * @returns {*[]} - список переменных в логическом выражении
  */
 function variable(string) {
-    const alp = ['!', '&', '|']
-    const array = Array.from(string)
+    const array = Array.from(string);
+    const alp = ["!", "&", "|"]
     let elements = []
+
     for (const element of array) {
         if (alp.indexOf(element) === -1) {
             elements.push(element)
         }
     }
+
     return elements
 }
 
 /**
- *
+ * Построение таблицы истинности
  * @param string - логическое выражение
  * @returns {{True: *[], False: *[]}} - таблица истинности
  */
@@ -25,9 +26,10 @@ function make_truth_table(string) {
     const array = variable(string)
     const save = string
     let truth_table = {
-        'True': [],
-        'False': []
+        "True": [],
+        "False": []
     }
+
     for (let x = 0; x < 2; x++) {
         for (let y = 0; y < 2; y++) {
             for (let z = 0; z < 2; z++) {
@@ -38,21 +40,51 @@ function make_truth_table(string) {
                     string = string.replaceAll(array[2],z.toString())
                     string = string.replaceAll(array[3],w.toString())
                     let expression = eval(string)
+
                     if (expression === 1) {
-                        let changes = truth_table['True']
+                        let changes = truth_table["True"]
                         changes.push([x,y,z,w])
-                        truth_table['True'] = changes
+                        truth_table["True"] = changes
                     }
+
                     else {
-                        let changes = truth_table['False']
+                        let changes = truth_table["False"]
                         changes.push([x,y,z,w])
-                        truth_table['False'] = changes
+                        truth_table["False"] = changes
                     }
                 }
             }
         }
     }
+
     return truth_table
 }
 
-console.log(make_truth_table(input))
+/**
+ * Получение ввода пользователя в HTML
+ * @returns {*} - ввод
+ */
+function get_expression(){
+    let input = document.getElementById("inputObject")
+    return input.value
+}
+
+/**
+ * Построение таблицы HTML
+ */
+function make_html_table() {
+    const truth_table = make_truth_table(get_expression())
+    document.querySelector(".center_table").innerHTML = `<table class="table"></table>`
+
+    for (let key in truth_table) {
+        let row = document.createElement("tr")
+        row.innerHTML = `<td colspan="2">${key}</td>`
+        document.querySelector(".center_table").appendChild(row)
+
+        for (let i = 0; i < truth_table[key].length; i++) {
+            let row = document.createElement("tr")
+            row.innerHTML = `<td>${truth_table[key][i]}</td>`
+            document.querySelector(".center_table").appendChild(row)
+        }
+    }
+}
